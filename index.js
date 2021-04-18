3d5467709888f3151a1a18f6f20b85e0cbd39285
Narcos85 <79701070+Narcos85@users.noreply.github.com>
2021-04-18 18:30:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const app = express();
+const url = require("url");
+
+
+
+function findCodeToId(data, id) {
+  var keys = Object.keys(data);
+  keys = keys.filter(key => data[key].id == id)[0];
+  keys = data[keys];
+  return keys;
+}
+
+function sortData(object) {
+  var keys = Object.keys(object);
+  var newData = {};
+  var arr = [];
+  keys.forEach(key => {// sup pothc :)
+    arr.push(key);
+  });
+  arr.reverse();
+  arr.forEach(key => {
+    newData[key] = object[key];
+  })
+  return newData;
+}
+
+app.get("/hata", (req, res) => {
+  res.render("hata", {
+    user: req.user,
+    statuscode: req.query.statuscode,
+    message: req.query.message
+  });
+});
+
+app.use((req, res) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  return res.redirect(
+    url.format({
+      pathname: "/hata",
+      query: {
+        statuscode: 404,
+        message: "Sayfa Bulunamadı"
+      }
+    })
+  );
+});
